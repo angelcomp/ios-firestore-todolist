cf592efe8c0e03bb751957d4d6c1c72011da8f3d
Angelica dos Santos <santosangelicassp@gmail.com>
2023-06-10 16:15:50 -0300

    initial commit

---
+++ ios-firestore-todolist/ios_firestore_todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_firestore_todolistApp.swift
+//  ios-firestore-todolist
+//
+//  Created by Angelica dos Santos on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_firestore_todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
